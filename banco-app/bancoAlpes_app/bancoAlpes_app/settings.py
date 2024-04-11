9c2560263973a9fbf9bc33ed0fffda70d28384ff
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-04-11 18:14:35 -0500

    Actualizado Dbs

--- banco-app/bancoAlpes_app/bancoAlpes_app/settings.py
+++ banco-app/bancoAlpes_app/bancoAlpes_app/settings.py
@@ -76,20 +76,14 @@ WSGI_APPLICATION = 'bancoAlpes_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } 
-}
-
-"""
-    ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'banco_db',
         'USER': 'banco_user',
         'PASSWORD': 'isis2503',
         'HOST': '10.128.0.52',
         'PORT': '',
-   
-"""
+    } 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
